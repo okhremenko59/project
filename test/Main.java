250c742e9c4680052065464347e5109bad1f2886
okhremenko59 <okhremenko@bk.ru>
2022-04-11 20:52:14 +0300

    test

--- test/Main.java
+++ test/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String[] arr = {"J","a","v","a","!"};
         for (int i = 0; i<arr.length; i++)
-        System.out.print(arr[i]);
+        System.out.print(arr[i]); //test comment
 
   }
     }
