450c97e97a9cfd4762bf9adb5db0468342b7ea91
okhremenko59 <okhremenko@bk.ru>
2022-04-11 20:08:05 +0300

    new project arr

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] arr = {"J","a","v","a","!"};
+        for (int i = 0; i<arr.length; i++)
+        System.out.print(arr[i]);
+
+  }
+    }
+
